dede74deafeb2998189dc342b67bc42a7afa664c
TheUglyDuwang <mattfriesen2000@gmail.com>
2023-12-02 13:14:41 -0800

    Copied Assignment class from Calendar branch, added room to dependencies

---
+++ app/src/main/java/group6/learnlock/model/Assignment.kt
@@ -0,0 +1,12 @@
+package group6.learnlock.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName="assignment_table")
+class Assignment(val course:String,val description:String,val dueDateTime: Long) {
+
+    @PrimaryKey(autoGenerate = true)
+    var id=0
+
+}
